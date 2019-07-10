984c93008a9da42ccb8a1b267177c29fe3858097
caroline dugan <caroline.dugan@gmail.com>
2019-07-10 08:56:43 -0700

    Initial Commit

---
+++ Contents.swift
@@ -0,0 +1,67 @@
+import UIKit
+
+//// Declare a new empty array that stores strings
+//var friendsOfKarlie = [String]()
+//
+////Write strings into my array
+//friendsOfKarlie = ["Michelle Obama", "Serena Williams", "T Swift", "Jimmy Fallon"]
+//
+////There are four friends -- Elements
+////Each have an assigned location
+////Michelle Obama is at location 0 or more specifically index 0
+////Jimmy Fallon is index 3
+//
+////print Michelle Obama
+//print(friendsOfKarlie[0])
+//
+//print(friendsOfKarlie[2])
+//
+////Add Josh Kushner to friends of karlie
+//friendsOfKarlie.append("Josh Kushner")
+//// Check if josh is at index 4
+//print(friendsOfKarlie[4])
+//
+////Replace serena williams with megan
+//friendsOfKarlie[1] = "Megan"
+//print(friendsOfKarlie[1])
+//
+////Remove Jimmy
+//friendsOfKarlie.remove(at: 3)
+//print(friendsOfKarlie[3])
+//
+////Challenge how to add at a specific location
+//
+//var roleModels =  [String]()
+//roleModels = ["Beyonce", "George Washington", "Sophia"]
+//
+//print(roleModels[2])
+//
+//roleModels.append("Tom Holland")
+//
+//print(roleModels[3])
+//
+//roleModels[2] = "Bob the Builder"
+//
+//print(roleModels[2])
+//
+//roleModels.remove(at: 1)
+//
+//print(roleModels)
+
+//I am declaring a dictionary
+var recipe = ["almond flour": "3 and 1/2 cups", "gluten-free oats": "2 cups","mini choclate chips": "1 cup"]
+
+//key/value pairs
+//Keys: almond flour etc
+//value: 3 and 1/2 cups etc
+
+// declare dictionary with out knowing data
+var bread : [String : String] = [:]
+print(bread)
+
+ adding flour + assigning value
+bread["Almond flour"]
+bread["gluten-free oats"]
+bread["chocolate chips"]
+print(bread)
+
